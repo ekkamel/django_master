edcedaeb0e27b88b241fa360848dc0be7ce2df9e
ekkamel <ekkamel@gmail.com>
2023-03-13 23:22:02 +0200

    App cretaed

---
+++ myproject/main/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals  # makes database adapted to all languages
+from django.db import models
+
+# Create your models here.
+
+
+class Main(models.Model):       # capital Main in order not to get confused with app name
+      
+    name = models.TextField()
+    
+    def __str__(self):
+        return self.main 
